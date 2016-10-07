41389e3fa2226f50d044c7a59c6edee76bbd0920
Lenore Alford <topplethepat@users.noreply.github.com>
2016-10-07 16:18:48 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+	$('#button1').click(function(){
+		window.location.href = "https://docs.google.com/document/d/1UiUGZQI5tG8LptkqGD_gF68yWM8sIbxOkk3281qE9dE/edit?usp=sharing";
+		console.log('y');
+	});
+
+	// $('#page1').hover(function(){
+	// 	$(.transl).css("color", "#FF7F50");
+	// }, function(){
+
+	// 	$(.transl).css("transform","translateX(300px)","transition.all 1.5s ease-in-out");
+	
+	// })
+});
+		
+
+	
