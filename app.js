60d3ddca24c1af1d1019db5deabc7d08fa74a75b
topplethepat <lenore.alford@gmail.com>
2016-10-19 13:08:24 -0700

    optimized images

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	$('#button1').click(function(){
 		window.location.href = "https://docs.google.com/document/d/1UiUGZQI5tG8LptkqGD_gF68yWM8sIbxOkk3281qE9dE/edit?usp=sharing";
-		console.log('y');
+		
 	});
 
 
